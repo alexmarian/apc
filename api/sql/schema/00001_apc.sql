0b83f1c585ba4940c20f53a1da5894525d5e1646
Alex Marian <alexandru.marian.ext@infocert.it>
2025-04-07 18:33:21 +0300

    first migration

---
+++ api/sql/schema/00001_apc.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE apc
+(
+    id               SERIAL PRIMARY KEY,
+    cadastral_number TEXT UNIQUE,
+    address          TEXT,
+    total_area       NUMERIC,
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE apc;
+-- +goose StatementEnd
